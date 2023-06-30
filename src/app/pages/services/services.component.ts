fde93e1325d2036f856cc3a69381c970b7d40fe7
Kitmediclinic <kit@mediclinic.com.au>
2023-06-30 14:09:33 +0200

    Updated Home and Services

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-services',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private metaTagService: Meta,private metaTitle: Title) { }
 
   ngOnInit(): void {
+
+    this.metaTitle.setTitle('พาบินไปศัลยกรรมที่เกาหลี | พร้อมโปรศัลยกรรมเกาหลี  Grandnewlife');
+    this.metaTagService.addTags([
+      { name: 'description', content: `แกรนด์ นิว ไลฟ์ เอเจนซี่ศัลยกรรมเกาหลี พร้อมพาคุณบินไปศัลยกรรมที่เกาหลีอย่างครบวงจร ในราคาที่คุณเอื้อถึงได้ ดูรายละเอียดได้ที่นี่` },
+    ]);
+
   }
 
 }
